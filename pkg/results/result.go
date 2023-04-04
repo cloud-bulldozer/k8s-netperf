80eb8392fec1a0298918483462242ca73a7e1262
Sai Sindhur Malleni <smalleni@redhat.com>
2023-04-03 19:56:31 -0500

    Fix RR results (#51)
    This patch does the following:
    1. Removes Latency result for stream test as it is not relevant
    2. Fixes a bug where the ThroughputSummary instead of LatencySummary
       was being used for summarizing P99 latency in the result table
    
    Signed-off-by: Sai Sindhur Malleni <smalleni@redhat.com>
    Co-authored-by: Joe Talerico (rook) <joe.talerico@gmail.com>

--- pkg/results/result.go
+++ pkg/results/result.go
@@ -194,20 +194,12 @@ func ShowRRResult(s ScenarioResults) {
 
 // ShowLatencyResult accepts NetPerfResults to display to the user via stdout
 func ShowLatencyResult(s ScenarioResults) {
-	testTypes := []string{"STREAM", "RR"}
-	for _, testType := range testTypes {
-		if !checkResults(s, testType) {
-			continue
-		}
-		table := initTable([]string{"Result Type", "Driver", "Scenario", "Parallelism", "Host Network", "Service", "Message Size", "Same node", "Duration", "Samples", "99%tile value"})
-		for _, r := range s.Results {
-			if r.Driver == "netperf" {
-				if strings.Contains(r.Profile, testType) {
-					avg, _ := Average(r.LatencySummary)
-					table.Append([]string{fmt.Sprintf("📊 %s Latency Results", strings.Title(strings.ToLower(testType))), r.Driver, r.Profile, strconv.Itoa(r.Parallelism), strconv.FormatBool(r.HostNetwork), strconv.FormatBool(r.Service), strconv.Itoa(r.MessageSize), strconv.FormatBool(r.SameNode), strconv.Itoa(r.Duration), strconv.Itoa(r.Samples), fmt.Sprintf("%f (%s)", avg, "usec")})
-				}
-			}
-		}
-		table.Render()
-	}
+        table := initTable([]string{"Result Type", "Scenario", "Parallelism", "Host Network", "Service", "Message Size", "Same node", "Duration", "Samples", "Avg 99%tile value"})
+        for _, r := range s.Results {
+                if strings.Contains(r.Profile, "RR") {
+                        p99, _ := Average(r.LatencySummary)
+                        table.Append([]string{"RR Latency Results", r.Profile, strconv.Itoa(r.Parallelism), strconv.FormatBool(r.HostNetwork), strconv.FormatBool(r.Service), strconv.Itoa(r.MessageSize), strconv.FormatBool(r.SameNode), strconv.Itoa(r.Duration), strconv.Itoa(r.Samples), fmt.Sprintf("%f (%s)", p99, "usec")})
+                }
+        }
+        table.Render()
 }
