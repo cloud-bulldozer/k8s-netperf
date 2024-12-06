74b59e1056eba7c6a62dec85a71e28a07995d251
Charles CAPORALI <ccaporal@redhat.com>
2024-12-06 16:10:05 +0100

    Adding support of UDN l2 primary using VMs Needed to be run on 4.18 and above, support l2bridge or passt binding method

--- pkg/k8s/kubernetes.go
+++ pkg/k8s/kubernetes.go
@@ -156,8 +156,9 @@ func DeployL2Udn(dynamicClient *dynamic.DynamicClient) error {
 			"spec": map[string]interface{}{
 				"topology": "Layer2",
 				"layer2": map[string]interface{}{
-					"role":    "Primary",
-					"subnets": []string{"10.0.0.0/24", "2001:db8::/60"},
+					"role":          "Primary",
+					"subnets":       []string{"10.0.0.0/24"},
+					"ipamLifecycle": "Persistent",
 				},
 			},
 		},
@@ -592,7 +593,7 @@ func ExtractUdnIp(s config.PerfScenarios) (string, error) {
 
 // launchServerVM will create the ServerVM with the specific node and pod affinity.
 func launchServerVM(perf *config.PerfScenarios, name string, podAff *corev1.PodAntiAffinity, nodeAff *corev1.NodeAffinity) error {
-	_, err := CreateVMServer(perf.KClient, serverRole, serverRole, *podAff, *nodeAff, perf.VMImage, perf.BridgeServerNetwork)
+	_, err := CreateVMServer(perf.KClient, serverRole, serverRole, *podAff, *nodeAff, perf.VMImage, perf.BridgeServerNetwork, perf.Udn, perf.UdnPluginBinding)
 	if err != nil {
 		return err
 	}
@@ -600,6 +601,7 @@ func launchServerVM(perf *config.PerfScenarios, name string, podAff *corev1.PodA
 	if err != nil {
 		return err
 	}
+
 	if strings.Contains(name, "host") {
 		perf.ServerHost, err = GetPods(perf.ClientSet, fmt.Sprintf("app=%s", serverRole))
 		if err != nil {
@@ -617,7 +619,7 @@ func launchServerVM(perf *config.PerfScenarios, name string, podAff *corev1.PodA
 
 // launchClientVM will create the ClientVM with the specific node and pod affinity.
 func launchClientVM(perf *config.PerfScenarios, name string, podAff *corev1.PodAntiAffinity, nodeAff *corev1.NodeAffinity) error {
-	host, err := CreateVMClient(perf.KClient, perf.ClientSet, perf.DClient, name, podAff, nodeAff, perf.VMImage, perf.BridgeClientNetwork)
+	host, err := CreateVMClient(perf.KClient, perf.ClientSet, perf.DClient, name, podAff, nodeAff, perf.VMImage, perf.BridgeClientNetwork, perf.Udn, perf.UdnPluginBinding)
 	if err != nil {
 		return err
 	}
