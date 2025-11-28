d0d8b74144a52d683427cc8540f7daa36a199c4c
Jose Castillo Lema <josecastillolema@gmail.com>
2025-11-28 13:53:38 +0100

    Added --ib-write-bw {nic}:{gid} option
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -56,6 +56,7 @@ type PerfScenarios struct {
 	BridgeNamespace     string
 	BridgeServerNetwork string
 	BridgeClientNetwork string
+	IbWriteBwParams     string
 	Sockets             uint32
 	Cores               uint32
 	Threads             uint32
